50805850944eb9d78e7abf19323eefe2c25de60d
Nara Davaasuren <naraamtm@gmail.com>
2022-08-22 10:28:59 -0600

    Changed text in About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,7 +14,7 @@ export default function About() {
       <div className="container">
         <img className="profile-picture" src={ImageProfile} alt="Profile" />
         <p>Full Stack Web Developer with a background of software developer and life-long dedication to
-          learning. Earned a plethora of experience and knowledge in financial sector. Effective at combining creativity and analytical problem-solving skills to develop user-friendly applications.</p>
+          learning. Accumulated a plethora of experience and knowledge in financial sector. Effective at combining creativity and analytical problem-solving skills to develop user-friendly applications.</p>
         <p>Recently earned a certificate in Full Stack Web Development from a Coding Bootcamp of the
           University of Denver. Result-oriented, self-starter whose drive and sense of urgency are tempered and disciplined by concern for the accuracy and quality of work.</p>
       </div>
